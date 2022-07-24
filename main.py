2524af5f64ee378b93860474b2481828ff182734
albertogg1 <85020182+albertogg1@users.noreply.github.com>
2022-07-24 15:32:41 +0000

    el bot hace un canal de configuración

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ import discord
 import requests
 import json
 import random
+from discord.ext import commands
 
 ##from keep_alive import keep_alive  # para mantener al bot online
 
 client = discord.Client()
-sad_words = ['suspenso', 'mal', 'agobiado']
 
+sad_words = ['suspenso', 'mal', 'agobiado']
 starter_encouragments = ['Ánimo!', 'Todo irá bien!']
 
 
@@ -22,6 +23,22 @@ def get_quote():
 @client.event
 async def on_ready():  # mierda que hay que poner
     print('We have logged in as {0.user}'.format(client))
+
+    guild_name = client.guilds[0]
+    print(guild_name)
+    text_channel = guild_name.text_channels
+    print(len(text_channel))
+
+    for i in range(0, len(text_channel)):
+        var = 0
+
+        if 'configuracion-pomodoro' == str(text_channel[i]):
+            # no se por que no funciona esta linea -> faltaba el str(), porque no son del mismo tipo
+            var = var + 1
+            break
+
+    if var == 0:
+        await guild_name.create_text_channel('configuracion-pomodoro')
 
 
 @client.event
@@ -44,6 +61,14 @@ async def on_message(message):  # funcion para los comandos
         if w in sad_words:
             await message.channel.send(random.choice(starter_encouragments))
 
+    if message.content.startswith('start'):
+        channel = ctx.author.voice.channel
+        await channel.connect()
+
+
+#@bot.command()
+#async def leave(ctx):
+#    await ctx.voice_client.disconnect()
 
 ##keep_alive()
 client.run(os.environ['TOKEN'])  # para correr el bot
