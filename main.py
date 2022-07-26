5073eedb3f3f98400ab8e12fd76ce8e3c3fe0a66
albertogg1 <85020182+albertogg1@users.noreply.github.com>
2022-07-26 08:46:29 +0000

    Antes de intentar cambiar todo a Bot

--- main.py
+++ main.py
@@ -6,18 +6,12 @@ import random
 from discord.ext import commands
 
 ##from keep_alive import keep_alive  # para mantener al bot online
+bot = commands.Bot(command_prefix='!')
 
 client = discord.Client()
 
 sad_words = ['suspenso', 'mal', 'agobiado']
 starter_encouragments = ['Ánimo!', 'Todo irá bien!']
-
-
-def get_quote():
-    response = requests.get('https://zenquotes.io/api/random')
-    json_data = json.loads(response.text)
-    quote = json_data[0]['q'] + ' -' + json_data[0]['a']
-    return (quote)
 
 
 @client.event
@@ -33,7 +27,6 @@ async def on_ready():  # mierda que hay que poner
         var = 0
 
         if 'configuracion-pomodoro' == str(text_channel[i]):
-            # no se por que no funciona esta linea -> faltaba el str(), porque no son del mismo tipo
             var = var + 1
             break
 
@@ -46,29 +39,34 @@ async def on_message(message):  # funcion para los comandos
     if message.author == client.user:
         return
 
-    if message.content.startswith('Hola pomo'):
-        await message.channel.send('Hola!')
+    if message.channel.name == 'configuracion-pomodoro':
 
-    elif message.content.startswith('Adios pomo'):
-        await message.channel.send('Adiós!')
+        if message.content.startswith('Hola pomo'):
+            await message.channel.send('Hola!')
 
-    elif message.content.startswith('!inspire'):
-        quote = get_quote()
-        await message.channel.send(quote)
+        elif message.content.startswith('Adios pomo'):
+            await message.channel.send('Adiós!')
 
-    for w in message.content.split(
-    ):  # split separa por los espacios en blanco
-        if w in sad_words:
-            await message.channel.send(random.choice(starter_encouragments))
+        for w in message.content.split(
+        ):  # split separa por los espacios en blanco
+            if w in sad_words:
+                await message.channel.send(random.choice(starter_encouragments)
+                                           )
 
-    if message.content.startswith('start'):
-        channel = ctx.author.voice.channel
-        await channel.connect()
+        if message.content.startswith('!start'):
+            # print(message.member)
+            channel = message.author.voice
+            print(channel)
+            await channel.connect()
+
+    await bot.process_commands(message)
 
 
-#@bot.command()
-#async def leave(ctx):
-#    await ctx.voice_client.disconnect()
+@bot.command()
+async def hola(ctx):
+    print('hola')
+
 
 ##keep_alive()
+
 client.run(os.environ['TOKEN'])  # para correr el bot
